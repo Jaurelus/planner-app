72636c8feda17eb00d92029153b7672f8a3ed87b
“Jaurelus” <hrqrdbrdzf@privaterelay.appleid.com>
2026-07-14 14:43:33 -0400

    Finance otw

--- frontend/src/screens/financeScreen/financeScreen.tsx
+++ frontend/src/screens/financeScreen/financeScreen.tsx
@@ -5,9 +5,8 @@ function FinanceScreen({ route }) {
   const { api } = route.params;
 
   return (
-    <View className="flex flex-1">
-      <Text>Finance</Text>
-      <PlaidLogin api={api}></PlaidLogin>
+    <View className="flex flex-1 items-center justify-center">
+      <Text>Coming Soon</Text>
     </View>
   );
 }
